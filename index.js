6174006cc209b3acdb895fcea81b20d1957060b9
Gaurav V <gaurav.v1395@gmail.com>
2018-12-26 14:09:48 +0530

    env

--- index.js
+++ index.js
@@ -3,14 +3,24 @@ const app = express();
 const { mongoose } = require('./config/db');
 const { routes } = require('./config/routes');
 const cors = require('cors');
- socket = require('socket.io');
+const path = require("path");
+const socket = require('socket.io');
 const port = 3001;
+require('dotenv').config()
 
 app.use(express.json());
 
 app.use(cors());
 
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+
 app.use('/', routes);
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
 
 app.listen(port, () => {
     console.log('listening to port', port);
